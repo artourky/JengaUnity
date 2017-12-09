a07af020fcec82275656c92dd749c787e41f902d
Arshd <arshad@sort6.com>
2017-12-09 20:54:35 +0200

    Added new Row & Light issue

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Logic : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			print("[Logic] Resarting Game");
+			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+		}
+	}
+
+}
