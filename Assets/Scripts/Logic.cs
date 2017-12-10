b2b70f1323dcda235ae722b628dfdccbb4cb084e
Arshd <arshad@sort6.com>
2017-12-10 17:12:11 +0200

    Creating Tower auto

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -2,13 +2,40 @@
 
 public class Logic : MonoBehaviour {
 
+	TowerCreation towerObj;
+
+	void Start()
+	{
+		print("[Logic] Press 'r' to reload Scene, 'c' to recreate Tower!");
+
+		if (towerObj == null)
+		{
+			towerObj = GetComponent<TowerCreation>();
+		}
+	}
+
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.R))
 		{
-			print("[Logic] Resarting Game");
-			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+			RestartLevel();
+		}
+
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			RecreateTower();
 		}
 	}
 
+	public void RestartLevel()
+	{
+		print("[Logic] Resarting Game");
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+
+	public void RecreateTower()
+	{
+		print("[Logic] recreating Tower");
+		towerObj.RecreateTower();
+	}
 }
---
+++ Assets/Scripts/TowerCreation.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TowerCreation : MonoBehaviour {
+
+	public GameObject row;
+	[Range(10, 20)]
+	public int noOfRows = 11;
+
+	public float startFixedY = -2.5f;
+
+	float startingY;
+
+	List<GameObject> rows;
+
+	// Use this for initialization
+	void Start () {
+		startingY = startFixedY;
+		rows = new List<GameObject>(noOfRows);
+		CreateTower();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateTower() 
+	{
+		print("[Tower] Creating!");
+
+		for (int i = 0; i < noOfRows; i++)
+		{
+			GameObject tmp = Instantiate(row, Vector3.zero, (i % 2 == 0) ? Quaternion.identity : Quaternion.Euler(0f, 90f, 0f), transform);
+			tmp.transform.localPosition = new Vector3(0, startingY++);
+			tmp.name = "Row#" + (i + 1);
+		}
+	}
+
+	public void RecreateTower() 
+	{
+		print("[Tower] recreating!");
+
+		for (int i = 0; i < rows.Count; i++)
+		{
+			DestroyImmediate(rows[i]);
+		}
+
+		startingY = startFixedY;
+
+		CreateTower();
+	}
+}
